dbe34e97abf73fee6292964c093c4d4d6dc15baf
thalycomp <thalycomp@gmail.com>
2020-11-18 17:23:43 -0300

    configurando multer para upload de imagens

---
+++ src/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+
+    console.log(`banco conectado ${conn.connection.host}`);
+    
+  } catch(err) {
+    console.error(`erro: ${err.message}`);
+    process.exit(1);
+  } 
+}
+
+export default connectDB;
---
+++ src/config/multer.js
@@ -0,0 +1,25 @@
+import multer from 'multer';
+import { extname, resolve } from 'path';
+
+const random = () => Math.floor(Math.random() * 10000 + 10000);
+
+export default {  
+  fileFilter: (req, file, cb) => {
+    console.log(req);
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+      return cb(new Error('Image: Incompatible format'));
+    }
+
+    return cb(null, true);
+  },
+  /*** ATENÇÃO: FALTA VALIDAR TAMANHOS!!! */
+  
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, resolve(__dirname, '..', '..', 'uploads'));
+    },
+    filename: (req, file, cb) => {
+      cb(null, `${Date.now()}_${random()}${extname(file.originalname)}`);
+    },
+  }),
+};
