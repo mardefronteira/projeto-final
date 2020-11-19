a9c70f5692e0cf7ce0af24ec700c425a467fbf06
Jardel Costa <jfcosta@ic.ufal.br>
2020-11-19 14:54:53 -0300

    middleware para checar se o usuário está logado

--- src/config/auth.js
+++ src/config/auth.js
@@ -2,16 +2,22 @@ import argon2 from "argon2";
 
 class auth {
   async hashPassword(password) {
-      try {
-        return argon2.hash(password);
-      } catch(err) {
-        /*Ver qual tipo de erro seria retornado aqui*/
-      }
+    try {
+      return argon2.hash(password);
+    } catch (err) {
+      /*Ver qual tipo de erro seria retornado aqui*/
+    }
   }
 
   async validadePassword(password, passwordhash) {}
 
-  authCheck(req, res) {}
+  authCheck(req, res) {
+    if (!req.session.isLoggedIn || !req.session.user.user_id) {
+      res.status(401).json({ error: "You are not authorized" });
+    } else {
+      res.status(200);
+    }
+  }
 }
 
-export default new auth;
+export default new auth();
