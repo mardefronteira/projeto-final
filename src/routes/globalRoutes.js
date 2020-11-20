615e19c4fc622a88d723c54b0284ee545428a9e9
Jardel Costa <jfcosta@ic.ufal.br>
2020-11-19 23:02:58 -0300

    adicionando middleware para autenticar usuario via token

--- src/routes/globalRoutes.js
+++ src/routes/globalRoutes.js
@@ -24,4 +24,7 @@ routes.post("/user", upload.none(), UserController.store);
 /*Rotas de Login*/
 routes.post("/signin", upload.none(), AuthController.signin);
 
+/*Rota para testar Auth*/
+routes.get("/", AuthController.authUser, function(req, res){ res.json({ id : req.userID })});
+
 export default routes;
