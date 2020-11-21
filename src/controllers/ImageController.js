ad4f1f05b05a8f8be3007dc4948f933408b54a4c
thalyta <thalycomp@gmail.com>
2020-11-21 12:35:47 -0300

    update de imagem de produto

--- src/controllers/ImageController.js
+++ src/controllers/ImageController.js
@@ -1,4 +1,5 @@
 import ImageProduct from "../models/ImageProduct";
+import Product from "../models/Product";
 
 import multer from "multer";
 import multerConfig from "../config/multer";
@@ -18,6 +19,27 @@ class ImageController {
       });
 
       return res.status(200).json(image);
+    });
+  }
+  async update(req, res) {
+    const { id } = req.params;
+    
+    const { image } = await Product.findById(id);
+
+    upload(req, res, async (error) => {
+      if (error) {
+        return res.status(400).json("Image: Incompatible format");
+      }
+      const { originalname, filename } = req.file;
+
+      const imageUpdate = await ImageProduct.findOneAndUpdate({ _id: image }, {
+        originalname,
+        filename
+      }, {
+        useFindAndModify: false,
+      });
+
+      return res.status(200).json(imageUpdate);
     });
   }
 }
